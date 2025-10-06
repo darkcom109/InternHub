ad4fdff18faaa3478029145ee596dd43df39bb55
darkcom109 <garciavalentino2006@gmail.com>
2025-10-06 15:45:10 +0100

    Added security hardening

--- internhub_project/internhub_project/settings.py
+++ internhub_project/internhub_project/settings.py
@@ -24,10 +24,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%_w_^15@^v$v2(gdeg7-28m0!@)#3anq))xr-&y!)7rz&$#5q*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = 'DENY'
+
 
 ALLOWED_HOSTS = []
 
